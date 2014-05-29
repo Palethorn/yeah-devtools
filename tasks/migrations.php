8b34b2c0a81da1974ebd085282a1aed2507f4bf1
David Cavar <wizzard405@gmail.com>
2014-05-29 20:19:07 +0200

    Migrations support completed

--- tasks/migrations.php
+++ tasks/migrations.php
@@ -3,96 +3,182 @@
 class migrations {
 
     public function execute($params) {
+        $this->initProperties($params);
+        if(!$this->checkParams()) {
+            return 1;
+        }
+
+        $this->initDb();
+        
+        $this->migrations_dir = $this->project .
+                DIRECTORY_SEPARATOR .
+                'data' .
+                DIRECTORY_SEPARATOR .
+                'migrations';
+
+        $this->migrations_version_file = $this->migrations_dir . DIRECTORY_SEPARATOR . 'version';
+        $action = $this->action;
+        return $this->$action($params);
+    }
+
+    public function initProperties($params) {
         $this->project = isset($params[1]) ? $params[1] : null;
         $this->action = isset($params[2]) ? $params[2] : null;
-        if(!file_exists($this->project) ||
-                !file_exists($this->project .
-                        DIRECTORY_SEPARATOR .
-                        'config' .
-                        DIRECTORY_SEPARATOR .
-                        'database.php')) {
-            echo "Not in a project directory" . PHP_EOL;
-            return 1;
-        }
-        if(!isset($this->action)) {
-            echo "Action not specified!" . PHP_EOL;
-            return 1;
-        }
-        if(!method_exists($this, $this->action)) {
-            echo "Action does not exist!" . PHP_EOL;
-            return 1;
-        }
-        $this->app_db_conf = require_once $this->project .
+        $this->migration_name = isset($params[3]) ? $params[3] : null;
+    }
+
+    public function checkParams() {
+        $db_conf_path = $this->project .
                 DIRECTORY_SEPARATOR .
                 'config' .
                 DIRECTORY_SEPARATOR .
                 'database.php';
+
+        if(!file_exists($this->project) || !file_exists($db_conf_path)) {
+            echo "Not in a project directory" . PHP_EOL;
+            return false;
+        }
+        if(!isset($this->action)) {
+            echo "Action not specified!" . PHP_EOL;
+            return false;
+        }
+        if(!method_exists($this, $this->action)) {
+            echo "Action does not exist!" . PHP_EOL;
+            return false;
+        }
+        return true;
+    }
+
+    public function initDb() {
+        $db_conf_path = $this->project .
+                DIRECTORY_SEPARATOR .
+                'config' .
+                DIRECTORY_SEPARATOR .
+                'database.php';
+
+        $this->app_db_conf = require_once $db_conf_path;
         $this->pdo = new PDO($this->app_db_conf['dsn'], $this->app_db_conf['db_user'], $this->app_db_conf['db_password'], array(
             \PDO::ATTR_PERSISTENT => true,
             \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
             \PDO::ATTR_EMULATE_PREPARES => false,
             \PDO::ATTR_STRINGIFY_FETCHES => false
         ));
-
-        $this->migrations_dir = $this->project .
-                DIRECTORY_SEPARATOR .
-                'data' .
-                DIRECTORY_SEPARATOR .
-                'migrations';
-        $this->migrations_version_file = $this->migrations_dir . DIRECTORY_SEPARATOR . 'version';
-        $action = $this->action;
-        return $this->$action($params);
     }
 
-    public function install() {
-        echo "Installing..." . PHP_EOL;
-        $q = 'create table if not exists migrations(' .
-                'id int(11) auto_increment, ' .
-                'created_at datetime, ' .
-                'name varchar(255), ' .
-                'primary key(id)' .
-                ')';
-        if($this->pdo->exec($q)) {
-            echo "Failed" . PHP_EOL;
-            return 1;
+    public function listMigrations($sorting_order = SCANDIR_SORT_ASCENDING) {
+        $files = scandir($this->migrations_dir, $sorting_order);
+        $migrations = array();
+        foreach($files as $file) {
+            if(strpos('.', $file) === 0) {
+                continue;
+            }
+            $parts = explode('.', $file);
+            $ext = end($parts);
+            if($ext !== 'php') {
+                continue;
+            }
+            echo "Found " . $file . PHP_EOL;
+            $migrations[] = $file;
         }
-        echo "Migrations installed." . PHP_EOL;
-        return 0;
+        return $migrations;
     }
 
     public function create() {
-        $this->migration_name = '_' . uniqid();
+        $this->migration_name = '_' . date('YmdHis');
         $this->renderTemplate('migration', $this->migrations_dir .
                 DIRECTORY_SEPARATOR . $this->migration_name . '.php');
-        $q = 'insert into migrations(created_at, name) values(\'' .
-                date('Y-m-d H:i:s') . '\', \'' . $this->migration_name . '\')';
-        $this->pdo->exec($q);
+        echo "Created " . $this->migration_name . PHP_EOL;
+        return 0;
     }
 
-    public function update() {
+    public function upgrade() {
+        $version = $this->getCurrentVersion();
+
+        $last_migration_time = strtotime($version);
+        $migrations = $this->listMigrations();
+
+        foreach($migrations as $migration) {
+            $migration_time = $this->getMigrationTime($migration);
+
+            if($last_migration_time >= $migration_time) {
+                continue;
+            }
+            $this->executeMigration($migration);
+            $last_executed = $migration;
+        }
+
+        $last_migration_time = $this->getMigrationTime($last_executed);
+
+        $this->setCurrentVersion(date('Y-m-d H:i:s', $last_migration_time));
+        return 0;
+    }
+
+    public function downgrade() {
+        if($this->migration_name === null) {
+            echo "Please specify version." . PHP_EOL;
+            return 1;
+        }
+        $version = $this->getCurrentVersion();
+
+        $last_migration_time = strtotime($version);
+        $migrations = $this->listMigrations(SCANDIR_SORT_DESCENDING);
+
+        $target_time = $this->getMigrationTime($this->migration_name);
+        foreach($migrations as $migration) {
+            $migration_time = $this->getMigrationTime($migration);
+            if($target_time >= $migration_time) {
+                $last_executed = $migration;
+                break;
+            }
+            $this->executeMigration($migration, 'down');
+        }
+
+        $last_migration_time = $this->getMigrationTime($last_executed);
+
+        $this->setCurrentVersion(date('Y-m-d H:i:s', $last_migration_time));
+        return 0;
+    }
+
+    public function getMigrationTime($migration) {
+        return strtotime(substr(explode('.', $migration)[0], 1));
+    }
+
+    public function getClass($migration) {
+        return explode('.', $migration)[0];
+    }
+
+    public function executeMigration($migration, $action = 'up') {
+        $file_path = $this->migrations_dir . DIRECTORY_SEPARATOR . $migration;
+
+        if(!file_exists($file_path)) {
+            return 1;
+        }
+
+        $class = $this->getClass($migration);
+
+        echo 'Running: ' . $class . PHP_EOL;
+
+        require_once $file_path;
+
+        $object = new $class();
+
+        if($this->pdo->exec($object->$action()) === false) {
+            echo "Error running migration " . $class . PHP_EOL;
+            return 1;
+        }
+        return 0;
+    }
+
+    public function getCurrentVersion() {
         $fp = fopen($this->migrations_version_file, 'r');
         $version = fread($fp, 19);
         fclose($fp);
-        if(strlen($version) > 0) {
-            $q = 'select * from migrations where created_at > \'' . $version . '\'';
-        } else {
-            $q = 'select * from migrations';
-        }
-        $r = $this->pdo->query($q);
-        $r = $r->fetchAll();
-        if(count($r) === 0) {
-            echo "No migrations" . PHP_EOL;
-            return 0;
-        }
-        foreach ($r as $migration) {
-            $class = $migration['name'];
-            echo 'Running: ' . $class . PHP_EOL;
-            require_once $this->migrations_dir . DIRECTORY_SEPARATOR . $class . '.php';
-            $mig = new $class();
-            $r = $this->pdo->query($mig->up());
-        }
+        return $version;
+    }
+
+    public function setCurrentVersion($version) {
         $fp = fopen($this->migrations_version_file, 'w');
-        fwrite($fp, $migration['created_at'], 19);
+        fwrite($fp, $version, 19);
         fclose($fp);
     }
 
