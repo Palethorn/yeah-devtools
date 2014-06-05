334cd48fdd3f5f76ead0d5634ce8ddb30e6703e8
David Cavar <wizzard405@gmail.com>
2014-06-05 20:27:04 +0200

    Directory check when creating new application create_app now adds default controller, view, layout and route when creating new application

--- tasks/create_app.php
+++ tasks/create_app.php
@@ -23,6 +23,7 @@ class create_app {
         $this->models_dir = $this->app_dir . DIRECTORY_SEPARATOR . 'models';
         $this->views_dir = $this->app_dir . DIRECTORY_SEPARATOR . 'views';
         $this->data_dir = $this->app_dir . DIRECTORY_SEPARATOR . 'data';
+        $this->layouts_dir = $this->views_dir . DIRECTORY_SEPARATOR . 'layouts';
 
         $this->createDirs();
         $this->renderTemplate('index', $this->web_dir . DIRECTORY_SEPARATOR . $this->app_name . '.php');
@@ -33,19 +34,42 @@ class create_app {
         $this->renderTemplate('paths', $this->config_dir . DIRECTORY_SEPARATOR . 'paths.php');
         $this->renderTemplate('routes', $this->config_dir . DIRECTORY_SEPARATOR . 'routes.php');
         $this->renderTemplate('schema', $this->data_dir . DIRECTORY_SEPARATOR . $this->app_name . '_schema.php');
+        $this->renderTemplate('HomeController', $this->controllers_dir . DIRECTORY_SEPARATOR . 'HomeController.php');
+        $this->renderTemplate('index_view', $this->views_dir . DIRECTORY_SEPARATOR . 'index.php');
+        $this->renderTemplate('default_layout', $this->layouts_dir . DIRECTORY_SEPARATOR . 'default.php');
     }
 
     private function createDirs() {
-        mkdir($this->app_dir);
-        mkdir($this->web_dir);
-        mkdir($this->config_dir, 0775, true);
-        mkdir($this->controllers_dir, 0775, true);
-        mkdir($this->models_dir, 0775, true);
-        mkdir($this->views_dir, 0775, true);
-        mkdir($this->data_dir, 0775, true);
+        if(!is_dir($this->app_dir)) {
+            mkdir($this->app_dir);
+        }
+        if(!is_dir($this->web_dir)) {
+            mkdir($this->web_dir);
+        }
+        if(!is_dir($this->config_dir)) {
+            mkdir($this->config_dir, 0775, true);
+        }
+        if(!is_dir($this->controllers_dir)) {
+            mkdir($this->controllers_dir, 0775, true);
+        }
+        if(!is_dir($this->models_dir)) {
+            mkdir($this->models_dir, 0775, true);
+        }
+        if(!is_dir($this->views_dir)) {
+            mkdir($this->views_dir, 0775, true);
+        }
+        if(!is_dir($this->layouts_dir)) {
+            mkdir($this->layouts_dir, 0775, true);
+        }
+        if(!is_dir($this->data_dir)) {
+            mkdir($this->data_dir, 0775, true);
+        }
     }
-    
-    public function renderTemplate($template_name, $destination) {
+
+    public function renderTemplate($template_name, $destination, $overwrite = false) {
+        if(file_exists($destination) && $overwrite === false) {
+            return;
+        }
         ob_start();
         require_once 'templates' . DIRECTORY_SEPARATOR . 'create_app' . DIRECTORY_SEPARATOR . $template_name . '.template';
         $rendered = ob_get_clean();
