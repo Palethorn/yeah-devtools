90bddf97e986ce3e33070d2562434537badfd285
David Cavar <wizzard405@gmail.com>
2017-02-11 19:32:51 +0100

    Schema inspection fixes

--- tasks/generate_schema.php
+++ tasks/generate_schema.php
@@ -8,31 +8,35 @@ class generate_schema {
             return 1;
         }
 
-        $db_conf = require_once $this->db_conf_path;
+        $db_conf = require_once $this->conf_path;
+        $db_conf = $db_conf['database'];
         $schema = $this->pickupSchemaFromDb($db_conf);
-
-        $s = '$schema = ' . var_export($schema, true) . ';';
-        $fp = fopen($this->schema_path, 'w');
-        fwrite($fp, '<?php' . PHP_EOL);
-        fwrite($fp, $s);
-        fclose($fp);
+        mkdir($this->schema_path, 0777, true);
+        chmod($this->schema_path, 0777);
+        foreach ($schema as $table => $config) {
+            $s = 'return ' . var_export($schema[$table], true) . ';';
+            $fp = fopen($this->schema_path . DIRECTORY_SEPARATOR . $table . '.php', 'w');
+            fwrite($fp, '<?php' . PHP_EOL);
+            fwrite($fp, $s);
+            fclose($fp);
+        }
     }
 
     private function init($params) {
         if(!isset($params[1])) {
-            echo "You didn't specify application name";
+            echo "You didn't specify application name" . PHP_EOL;
             return 1;
         }
         $this->app_name = $params[1];
-        
+
         $work_dir = getcwd();
-        $this->db_conf_path = $work_dir . DIRECTORY_SEPARATOR . $this->app_name . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'database.php';
-        if(!file_exists($this->db_conf_path)) {
+        $this->conf_path = $work_dir . DIRECTORY_SEPARATOR . $this->app_name . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.php';
+        if(!file_exists($this->conf_path)) {
             echo "Database configuration not found. Are you inside project directory?" . PHP_EOL;
             return 1;
         }
 
-        $this->schema_path = $work_dir . DIRECTORY_SEPARATOR . $this->app_name . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . $this->app_name . '_schema.php';
+        $this->schema_path = $work_dir . DIRECTORY_SEPARATOR . $this->app_name . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'schema';
     }
 
     private function pickupSchemaFromDb($db_conf) {
@@ -52,7 +56,8 @@ class generate_schema {
     }
 
     private function getTables($db_conf) {
-        $db = new PDO($db_conf['dsn'], $db_conf['db_user'], $db_conf['db_password']);
+        $dsn = sprintf('mysql:host=%s;dbname=%s', $db_conf['host'], $db_conf['database']);
+        $db = new PDO($dsn, $db_conf['username'], $db_conf['password']);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $stmt = $db->prepare('SHOW TABLES');
         $stmt->execute();
@@ -60,7 +65,8 @@ class generate_schema {
     }
 
     private function describeTable($db_conf, $table) {
-        $db = new PDO($db_conf['dsn'], $db_conf['db_user'], $db_conf['db_password']);
+        $dsn = sprintf('mysql:host=%s;dbname=%s', $db_conf['host'], $db_conf['database']);
+        $db = new PDO($dsn, $db_conf['username'], $db_conf['password']);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $stmt = $db->prepare('DESCRIBE ' . $table);
         $stmt->execute();
